e8b2fd007c231836efa4994741b91436626240d2
SK1723 <kumar.sneha1723@gmail.com>
2017-07-13 18:00:52 -0400

    Add files via upload

---
+++ MyStack.java
@@ -0,0 +1,40 @@
+package edu.cornell.sneha;
+
+public class MyStack {
+	private Node top;
+	private int length;
+	public void push(Person p){
+		top= new Node(p,top);
+		length++;
+	}
+	public Person pop(){
+		if (!isEmpty()){
+			Person temp=top.getData();
+			top=top.getNext();
+			length--;
+			return temp;
+		}
+		else
+		{
+			System.out.println("Stack is empty!");
+			return null;
+		}
+	}
+	
+	public int getLength(){
+		return this.length; 
+	}
+	public boolean isFull(){
+		return false; 
+	}
+	public boolean isEmpty(){
+		return ( length == 0 );
+	}
+	public MyStack (int n){
+		length=0; 
+	}
+	public MyStack(){
+		this (0);
+	}
+
+}
---
+++ Person.java
@@ -0,0 +1,11 @@
+package edu.cornell.sneha;
+
+
+public class Person {
+	protected String person;
+	protected Person(String input){
+		this.person=input;
+	}
+	
+}
+	
